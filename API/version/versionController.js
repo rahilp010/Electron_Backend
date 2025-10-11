58a39003680f832535cf5ee894fbb1ebb5a66ccb
Rahil Patel <rahilp704@gmail.com>
2025-10-11 14:30:49 +0530

    down to v1.2.0

--- API/version/versionController.js
+++ API/version/versionController.js
@@ -1,7 +1,7 @@
 const versionController = (req, res) => {
     res.json(
         {
-            "version": '1.3.0',
+            "version": '1.2.0',
             "url": "https://www.dropbox.com/scl/fi/8mw9v27gtihh5qwaeptf3/Electron-Release-v1.3.0.0.zip?rlkey=4ck0seegsb5l3krh2zurgv4yf&st=7eve8nh9&dl=1",
             "status": "success",
             "changeLog": "Added Version Update"
