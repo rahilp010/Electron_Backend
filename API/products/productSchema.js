5f04aa52effb31c7818143e1e8a730afc073dd76
Rahil Patel <rahilp704@gmail.com>
2025-12-07 01:54:52 +0530

    added new fields in clients

--- API/products/productSchema.js
+++ API/products/productSchema.js
@@ -5,8 +5,8 @@ const productSchema = new mongoose.Schema(
         productName: { type: String, required: true },
         productPrice: { type: Number, required: true },
         productQuantity: { type: Number, default: 0 },
-        clientName: { type: String },
-        assetType: { type: String },
+        clientName: { type: String, index: true },
+        assetType: { type: String, index: true },
         saleHSN: { type: String },
         purchaseHSN: { type: String },
         taxRate: { type: Number },
@@ -16,7 +16,10 @@ const productSchema = new mongoose.Schema(
         addParts: { type: Array },
     },
     { timestamps: true }
-);
+)
+productSchema.index({ productName: "text" });
+productSchema.index({ createdAt: -1 });
+productSchema.index({ assetType: 1, clientName: 1 });
 
 const Product = mongoose.model("Product", productSchema);
 export default Product;
