4d4e5074030ba6998f17c073be4682cafcdda95f
Rahil Patel <rahilp704@gmail.com>
2026-02-27 23:30:39 +0530

    Version Update

--- API/version/versionController.js
+++ API/version/versionController.js
@@ -2,7 +2,7 @@ const versionController = (req, res) => {
     res.json(
         {
             "version": '1.2.0',
-            "url": "https://www.dropbox.com/scl/fi/8mw9v27gtihh5qwaeptf3/Electron-Release-v1.3.0.0.zip?rlkey=4ck0seegsb5l3krh2zurgv4yf&st=5jhapuj5&dl=1",
+            "url": "https://www.dropbox.com/scl/fi/42gj17a24f2wgm1ie3nxl/electron.exe?rlkey=798i6xnpza1oai8e9fxkqopfg&st=dqfk33yz&dl=1",
             "status": "success",
             "changeLog": "Added Version Update"
         }
